995085ac9b035ec7f59e22f06244298c8b91423b
Shoter99 <shoter998@gmail.com>
2021-05-10 17:45:42 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componets/header/header.component';
+import { ButtonComponent } from './componets/button/button.component';
+import { TasksComponent } from './componets/tasks/tasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule
